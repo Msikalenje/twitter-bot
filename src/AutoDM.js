45f8895b94965de4e972907ce5f3a4c059ae4f74
sikalenje <45860310+sikalenje@users.noreply.github.com>
2018-12-14 00:38:34 +0200

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,8 +41,8 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
-  // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
+  return `Hi ${name} Turn on my notifications for 247 gain tweets \n Happy ${dayName} 😊😊 `; // your message
+  // My message   return `Hi ${name} Turn on my notifications for 247 gain tweets. Click on the bell icon to do so.\n Happy Gain 😊  \n Happy ${dayName} 😊😊 `;
 };
 
 module.exports = AutoDM;
